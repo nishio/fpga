7912b2ff6fb713e9bfeeae28ee26be09352b835b
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2013-04-21 22:42:10 +0900

    conway

---
+++ make_conwat_rule.py
@@ -0,0 +1,13 @@
+for i in range(512):
+    bits = '{0:09b}'.format(i)
+    s = bits.count('1')
+    c = int(bits[4])
+    s -= c
+    if s == 2:
+        newvalue = c
+    if s == 3:
+        newvalue = 1
+    else:
+        newvalue = 0
+
+    print "\t\t\t9'b{0:s}: newvalue = 1'b{1:d};".format(bits, newvalue)
