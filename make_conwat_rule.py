e97dc668376d1e5fbd74493fed21d8102fe1d6d6
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2013-04-23 00:57:09 +0900

    fix bug (again)

--- make_conwat_rule.py
+++ make_conwat_rule.py
@@ -1,13 +1,13 @@
-for i in range(1024):
+for i in range(512):
     bits = '{0:09b}'.format(i)
     s = bits.count('1')
     c = int(bits[4])
     s -= c
     if s == 2:
         newvalue = c
-    if s == 3:
+    elif s == 3:
         newvalue = 1
     else:
         newvalue = 0
 
-    print "\t\t\t9'b{0:s}: newvalue = 1'b{1:d};".format(bits, newvalue)
+    print "\t\t\t9'b{0:s}_{1:s}_{2:s}: wdata = 1'b{3:d};".format(bits[:3], bits[3:6], bits[6:], newvalue)
