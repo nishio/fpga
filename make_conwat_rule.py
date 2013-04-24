7f43a5e6f072861968f51f93315e52c38118a2b5
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2013-04-24 21:45:48 +0900

    omit 0

--- make_conwat_rule.py
+++ make_conwat_rule.py
@@ -1,3 +1,6 @@
+def print_rule(bits, newvalue):
+    print "\t\t\t9'b{0:s}_{1:s}_{2:s}: wdata = 1'b{3:d};".format(bits[:3], bits[3:6], bits[6:], newvalue)
+
 for i in range(512):
     bits = '{0:09b}'.format(i)
     s = bits.count('1')
@@ -5,9 +8,14 @@ for i in range(512):
     s -= c
     if s == 2:
         newvalue = c
+        if newvalue:
+            print_rule(bits, 1)
     elif s == 3:
         newvalue = 1
+        if c:
+            bits = bits[:4] + 'x' + bits[5:]
+            print_rule(bits, 1)
     else:
-        newvalue = 0
+        pass
 
-    print "\t\t\t9'b{0:s}_{1:s}_{2:s}: wdata = 1'b{3:d};".format(bits[:3], bits[3:6], bits[6:], newvalue)
+print "\t\t\tdefault: wdata = 1'b0;"
